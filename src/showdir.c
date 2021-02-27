346b16b4eb67763247595740beeb67151421524c
Matheus Rambo <matheusrambo24@gmail.com>
2021-02-27 19:12:19 -0300

    Fix handling errors, depth dirs and depth args

--- src/showdir.c
+++ src/showdir.c
@@ -6,17 +6,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <errno.h>
 
 #include "command.h"
 
 #define VERSION "0.0.1"
-#define MAX_BUFFER_SIZE 150
+#define MAX_BUFFER_SIZE 500
 
 DIR * open_directory_stream(char *);
 void close_directory_stream(DIR *);
 void show_directory_entries(char *, unsigned int, unsigned int);
+void handle_error(void);
 
-const char *short_options = "n:hvd";
+const char *short_options = "n:hvd:";
 struct option long_options[] = {
     { "help",      no_argument,       NULL, 'h' },
     { "name",      required_argument, NULL, 'n' },
@@ -37,7 +39,7 @@ int main(int argc, char *argv[]) {
     char directory_name[MAX_BUFFER_SIZE];
     memset(directory_name, 0, MAX_BUFFER_SIZE);
     bool tree_flag = false;
-    unsigned int max_depth = 1;
+    unsigned int max_depth = 3;
 
     while ( ( option = getopt_long(argc, argv, short_options, long_options, NULL)) != -1 ) {
         switch (option) {
@@ -65,7 +67,7 @@ int main(int argc, char *argv[]) {
     }
 
     if(strlen(directory_name) == 0) {
-        fprintf(stderr, "The filename must be provided!");
+        fprintf(stderr, "The filename must be provided!\n");
         return EXIT_FAILURE;
     }
 
@@ -77,6 +79,7 @@ DIR * open_directory_stream(char *name) {
     DIR *directory = opendir(name);
     if(directory == NULL) {
         fprintf(stderr, "Could not open the directory: %s.\n", name);
+        handle_error();
         exit(EXIT_FAILURE);
     }
     return directory;
@@ -94,17 +97,38 @@ void show_directory_entries(char *directory_name, unsigned int depth, unsigned i
     DIR *directory = open_directory_stream(directory_name);
     struct dirent *entry = readdir(directory);
     while(entry != NULL) {
-        puts(entry->d_name);
+
         // Ignore .. AND .. directories
         // FIXME: If the user has a directory that starts with .., we will ignore it ...
         if(entry->d_name[0] == '.' && (entry->d_name[1] == '.' || entry->d_name[1] == '\0')) goto next;
 
+        unsigned int tabs = depth * 2; /* White spaces and - */
+        if(tabs > 0) {
+            char tab[tabs + 1];
+            tab[tabs] = '\0';
+            for(unsigned int index = 0; index < tabs; index++) {
+
+                if(index < depth ) {
+                    tab[index] = ' ';
+                } else {
+                    tab[index] = '-';
+                }
+            } 
+            printf(tab);           
+        } 
+
+        puts(entry->d_name);
+
         // TODO: There are more types ...
         bool is_dir = entry->d_type == DT_DIR;
-        for(unsigned int index = depth; index < max_depth; index++) printf("-");
-        puts(entry->d_name);
-        if(is_dir && depth < max_depth) {
-            show_directory_entries(entry->d_name, depth + 1, max_depth);
+
+        if(is_dir && depth < max_depth) {            
+            unsigned int path_dir_size = strlen(directory_name) + strlen(entry->d_name) + 2;
+            char inner_directory[path_dir_size];
+            strcpy(inner_directory, directory_name);
+            strcat(inner_directory, entry->d_name);
+            strcat(inner_directory, "/");
+            show_directory_entries(inner_directory, depth + 1, max_depth);
         }
         next:
             entry = readdir(directory);
@@ -129,4 +153,20 @@ void help(char *name, FILE *file) {
 
 void version(char *bin) {
     printf("%s Version: %s.\n", bin, VERSION);
+}
+
+void handle_error(void) {
+    char *message;
+    switch(errno) {
+        case EACCES:
+            message = "Permission denied!\n";
+            break;
+        case ENOENT:
+            message = "Directory does not exists!\n";
+            break;
+        default:
+            message = "Unknown error!\n";
+            break;
+    }
+    fprintf(stderr, message);
 }
