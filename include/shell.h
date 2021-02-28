f0a7a655d2e9f4a032b7f632bba588f2ebc3fd68
Matheus Rambo <matheusrambo24@gmail.com>
2021-02-28 13:26:13 -0300

    Add: New functions to handle the user input

--- include/shell.h
+++ include/shell.h
@@ -6,9 +6,22 @@
 
 
 /**
- * Creates a child process
+ * Create a child process
  */  
 pid_t create_child(void);
+
+
+/**
+ * Create the command line arguments for the given input
+ * The caller must free the memory after the use.
+ */   
+char ** create_command_line_arguments(char *);
+
+/**
+ *  Handle the input.   
+ *  Creates a child process if necessary, exit, and so on...
+ */ 
+void handle_command_line(char *[]);
 
 
 
--- shell.c
+++ shell.c
@@ -9,7 +9,10 @@
 #define MAX_BUFFER_SIZE 150
 
 /** The name of the shell. */
-#define SHELL "ramboso_xterm-$$$ "
+#define SHELL "ramboso_xterm-$ "
+
+static char binaries_path[50];
+static char *environment_variables[1] = {NULL};
 
 pid_t create_child(void) {
     const pid_t pid = fork();
@@ -29,7 +32,7 @@ int main(int argc, char *argv[], char *envp[]) {
         exit(EXIT_FAILURE);
     }
     // Where the binaries are
-    const char *binaries_path = argv[1];
+    strcpy(binaries_path, argv[1]);
 
     // Buffer to read from stdin
     char buffer[MAX_BUFFER_SIZE];
@@ -40,48 +43,11 @@ int main(int argc, char *argv[], char *envp[]) {
         fflush(stdin);
         fgets(buffer, MAX_BUFFER_SIZE, stdin);
         size_t length = strlen(buffer);
-        buffer[length - 1] = ' '; // removes the line feed and puts an extra space .
+        buffer[length - 1] = '\0'; // removes the line feed.
 
-        const pid_t pid = create_child();
-        if(pid == 0) {
-            // child 
-
-            // Count how many args are there, plus one to the last argument that must be null.
-            unsigned int size = count_args(buffer) + 1;
-
-            // creates a temporary array to store
-            char *args[size];
-
-            // puts null into the last index
-            args[size - 1] = NULL;
-
-            // delimiter
-            const char *delimiter = " ";
-            
-            // to manage the index to store the element
-            unsigned int index = 0;
-            
-            // gets the element
-            char *token = strtok(buffer, delimiter);
-
-            while(token != NULL) {
-                args[index++] = token;
-                token = strtok(NULL, delimiter);            
-            }
-            
-            char binary_file[strlen(binaries_path) + strlen(args[0]) + 1];
-            strcpy(binary_file, binaries_path);
-            strcat(binary_file, args[0]);
-            execve(binary_file, args, envp);
-            perror("Error with execve.\n");
-            return EXIT_FAILURE;
-
-        } else {
-            // parent
-            // man wait
-            waitid(P_PID, pid, NULL, WEXITED);
-        }
-
+        char **cmd = create_command_line_arguments(buffer);
+        handle_command_line(cmd);
+        free(cmd);
     }
 
     return EXIT_SUCCESS;
@@ -97,3 +63,57 @@ unsigned int count_args(char *arg) {
     return size;
 }
 
+void handle_command_line(char *input[]) {
+
+    if(strcmp(input[0], "exit") == 0) {
+        exit(EXIT_SUCCESS);
+    }
+
+    const pid_t pid = create_child();
+    if(pid == 0) {
+        // child 
+        
+        char binary_file[strlen(binaries_path) + strlen(input[0]) + 1];
+        strcpy(binary_file, binaries_path);
+        strcat(binary_file, input[0]);
+        execve(binary_file, input, environment_variables);
+        perror("Error with execve.\n");
+        exit(EXIT_FAILURE);
+
+    } else {
+
+        // parent
+        // man wait
+        waitid(P_PID, pid, NULL, WEXITED);
+    }
+}
+
+char ** create_command_line_arguments(char *input) {
+
+    // Count how many args are there, plus one to the last argument that must be null.
+    unsigned int size = count_args(input) + 1;
+
+    // creates an array to store the arguments
+    char ** args = malloc(sizeof(char *) * size);
+
+    // puts null into the last index
+    args[size - 1] = NULL;
+
+    // delimiter
+    const char *delimiter = " ";
+    
+    // to manage the index to store the element
+    unsigned int index = 0;
+    
+    // gets the element
+    char *token = strtok(input, delimiter);
+
+    while(token != NULL) {
+        size_t bytes_to_alloc = sizeof(char) * (strlen(token) + 1);
+        args[index] = malloc(bytes_to_alloc);
+        strcpy(args[index++], token);
+        token = strtok(NULL, delimiter);            
+    }
+
+    return args;
+}
